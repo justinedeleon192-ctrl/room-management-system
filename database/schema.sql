6ef9cef3dd941a4fbe72602984c70c9ad4c98cd7
Kevin Bueno <KevzBueno101@users.noreply.github.com>
2026-05-22 15:00:23 +0800

    new

--- database/schema.sql
+++ database/schema.sql
@@ -2,7 +2,7 @@
 -- Compatible with XAMPP MySQL
 -- Created for PHP migration from localStorage system
 
--- Create database
+-- Create databaseas
 CREATE DATABASE IF NOT EXISTS school_system;
 USE school_system;
 
